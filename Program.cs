17f05c24887e44b2ecfa3b5344401eb751492ec3
Anderson Lima <anderson.lima-ext@olxbr.com>
2022-03-28 15:37:28 -0300

    refactor: add swagger edit config

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using jobs_application_api.Persistence;
 using jobs_application_api.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,16 +24,27 @@ builder.Services.AddScoped<IJobVacancyRepository, JobVacancyRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo{
+        Title = "Job Application Api",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Anderson Lima",
+            Email = "anderson.rl.tr@gamil.com",
+            Url = new Uri("https://www.linkedin.com/in/anderson-r-lima/")
+        }
+    });
+    var xmlFile = $"jobs-application-api.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
